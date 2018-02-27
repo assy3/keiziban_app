8921bcdc50c3f86a991bd81c57220820f72f6d4a
assy3 <b1748628@planet.kanazawa-it.ac.jp>
2018-02-27 15:21:09 +0900

    骨組みつくった

---
+++ db/migrate/20180227060313_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[5.1]
+  def change
+    create_table :messages do |t|
+      t.text :name
+      t.text :email
+      t.text :context
+
+      t.timestamps
+    end
+  end
+end
